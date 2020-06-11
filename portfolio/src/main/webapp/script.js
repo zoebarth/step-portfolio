6d700527540da583c19a86381d9aba3447d09ff1
Zoe Barth <zoeb.barth@gmail.com>
2020-06-11 19:12:53 +0000

    create map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -107,4 +107,12 @@ function handleSubmit(e) {
   $(this).find('input,textarea').val('');
 }
 
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 37.422, lng: -122.084 },
+    zoom: 16,
+  });
+}
+
 $('#comment-form').submit(handleSubmit);
